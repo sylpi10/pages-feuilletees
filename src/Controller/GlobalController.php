131b840c0a49f2374adee16ddbac39a8723cfc6b
sylpi <syl.pillet@hotmail.fr>
2020-10-31 23:12:24 +0100

    add texts

---
+++ src/Controller/GlobalController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GlobalController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('global/index.html.twig', [
+        ]);
+    }
+
+
+    /**
+     * @Route("/poèmes", name="poemes")
+     */
+    public function poemes(): Response
+    {
+        return $this->render('global/poemes.html.twig', [
+           
+        ]);
+    }
+    /**
+     * @Route("/souvenirs", name="souvenirs")
+     */
+    public function souvenirs(): Response
+    {
+        return $this->render('global/souvenirs.html.twig', [
+           
+        ]);
+    }
+    /**
+     * @Route("/pages", name="pages-feuilletées")
+     */
+    public function pages(): Response
+    {
+        return $this->render('global/pages.html.twig', [
+           
+        ]);
+    }
+
+    /**
+     * @Route("/mémoires", name="mémoires")
+     */
+    public function memoires(): Response
+    {
+        return $this->render('global/mémoires.html.twig', [
+           
+        ]);
+    }
+    /**
+     * @Route("/rouviere", name="rouviere")
+     */
+    public function rouviere(): Response
+    {
+        return $this->render('global/rouviere.html.twig', [
+           
+        ]);
+    }
+    /**
+     * @Route("/liens", name="liens")
+     */
+    public function liens(): Response
+    {
+        return $this->render('global/liens.html.twig', [
+           
+        ]);
+    }
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('global/contact.html.twig', [
+           
+        ]);
+    }
+}
